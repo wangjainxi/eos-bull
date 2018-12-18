2c9079ccd24cf8565207cd2b0097aef51f4e5fc8
guoqingwen <qingwenje2008@163.com>
2018-12-18 16:16:43 +0800

    add trading view

---
+++ src/web/views/mex/tradingView/index.vue
@@ -0,0 +1,16 @@
+
+
+ <template>
+   <div>
+     <TVChartContainer/>
+   </div>
+ </template>
+ <script>
+ import  TVChartContainer  from   '../../../../components/TVChartContainer/index.vue';
+ export default {
+   name:'trading-view',
+   components:{
+   TVChartContainer,
+   }
+ }
+ </script>
